4fd4bbb94fe776ea48a3bc96fd2a6bf0a15ed18f
Max Kobzar <nimbus2219@gmail.com>
2022-10-01 01:12:42 +0300

    update to git

---
+++ script.js
@@ -0,0 +1,11 @@
+function showPairscontrolArea() {
+    var x = document.getElementById("pairscontrol");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  
